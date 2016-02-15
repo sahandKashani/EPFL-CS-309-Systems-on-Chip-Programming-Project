bb98913782306f2ff4b64d37a900d98b096a8452
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2016-02-15 18:27:46 +0100

    [lab1] tmp commit of (incomplete and untested) mcp3204_spi.vhd

--- lab1/hw/hdl/mcp3204/hdl/mcp3204_spi.vhd
+++ lab1/hw/hdl/mcp3204/hdl/mcp3204_spi.vhd
@@ -4,69 +4,196 @@ use ieee.numeric_std.all;
 
 entity mcp3204_spi is
     port(
-        clk   : in  std_logic;
-        reset : in  std_logic;
-        busy  : out std_logic;
-        start : in  std_logic;
-        done  : out std_logic;
-        data  : out std_logic_vector(11 downto 0);
-        CS_N  : out std_logic;
-        MOSI  : out std_logic;
-        MISO  : in  std_logic
+        -- 50 MHz
+        clk        : in  std_logic;
+        reset      : in  std_logic;
+        busy       : out std_logic;
+        start      : in  std_logic;
+        channel    : in  std_logic_vector(1 downto 0);
+        data_valid : out std_logic;
+        data       : out std_logic_vector(11 downto 0);
+
+        -- 1 MHz
+        SCLK       : out std_logic;
+        CS_N       : out std_logic;
+        MOSI       : out std_logic;
+        MISO       : in  std_logic
     );
 end mcp3204_spi;
 
 architecture rtl of mcp3204_spi is
-    type state is (STATE_CS_HIGH, STATE_CS_LOW, STATE_START, STATE_SGL, STATE_D2, STATE_D1, STATE_D0, STATE_SAMPLE, STATE_NULL, STATE_D_IN);
+    signal reg_clk_divider_counter : unsigned(4 downto 0) := (others => '0'); -- need to be able to count until 24
+    signal reg_spi_en              : std_logic            := '0'; -- pulses every 0.5 MHz
+    signal reg_falling_edge_sclk   : std_logic            := '0';
 
-    signal reg_state, next_reg_state : state := STATE_CS_HIGH;
+    -- 1 MHz (not actuall "clocked" at 1 MHz, but rather "enabled" every 1 MHz)
+    type state is (STATE_CS_N_HIGH, STATE_CS_N_LOW, STATE_START, STATE_SGL, STATE_D2, STATE_D1, STATE_D0, STATE_SAMPLE, STATE_NULL, STATE_D_IN);
+    signal reg_state, next_reg_state : state := STATE_CS_N_HIGH;
+
+    signal reg_channel, next_reg_channel           : std_logic_vector(channel'range) := (others => '0');
+    signal reg_data_counter, next_reg_data_counter : unsigned(3 downto 0)            := (others => '0'); -- need to be able to count until 11
+
+    -- registered outputs
+    signal reg_busy, next_reg_busy             : std_logic                    := '0';
+    signal reg_data_valid, next_reg_data_valid : std_logic                    := '0';
+    signal reg_data, next_reg_data             : std_logic_vector(data'range) := (others => '0');
+    signal reg_sclk                            : std_logic                    := '0';
+    signal reg_cs_n, next_reg_cs_n             : std_logic                    := '0';
+    signal reg_mosi, next_reg_mosi             : std_logic                    := '0';
+
 begin
-    process(clk, reset)
+    busy       <= reg_busy;
+    data_valid <= reg_data_valid;
+    data       <= reg_data;
+    SCLK       <= reg_sclk;
+    CS_N       <= reg_cs_n;
+    MOSI       <= reg_mosi;
+
+    clk_divider_generation : process(clk, reset)
     begin
         if reset = '1' then
-            reg_state <= STATE_CS_HIGH;
+            reg_clk_divider_counter <= (others => '0');
         elsif rising_edge(clk) then
-            reg_state <= next_reg_state;
+            reg_clk_divider_counter <= reg_clk_divider_counter + 1;
+            reg_spi_en              <= '0';
+            reg_falling_edge_sclk   <= '0';
+
+            if reg_clk_divider_counter = 24 then
+                reg_clk_divider_counter <= (others => '0');
+                reg_spi_en              <= '1';
+
+                if reg_SCLK = '1' then
+                    reg_falling_edge_sclk <= '1';
+                end if;
+            end if;
         end if;
     end process;
 
-    process(reg_state)
+    SCLK_generation : process(clk, reset)
     begin
-        next_reg_state <= reg_state;
+        if reset = '1' then
+            reg_SCLK <= '0';
+        elsif rising_edge(clk) then
+            if reg_spi_en = '1' then
+                reg_SCLK <= not reg_SCLK;
+            end if;
+        end if;
+    end process;
+
+    STATE_LOGIC : process(clk, reset)
+    begin
+        if reset = '1' then
+            reg_state        <= STATE_CS_N_HIGH;
+            reg_channel      <= (others => '0');
+            reg_data_counter <= (others => '0');
+            reg_busy         <= '0';
+            reg_data_valid   <= '0';
+            reg_data         <= (others => '0');
+            reg_cs_n         <= '1';
+            reg_mosi         <= '0';
+        elsif rising_edge(clk) then
+            if reg_falling_edge_sclk = '1' then
+                reg_state        <= next_reg_state;
+                reg_channel      <= next_reg_channel;
+                reg_data_counter <= next_reg_data_counter;
+                reg_busy         <= next_reg_busy;
+                reg_data_valid   <= next_reg_data_valid;
+                reg_data         <= next_reg_data;
+                reg_cs_n         <= next_reg_cs_n;
+                reg_mosi         <= next_reg_mosi;
+            end if;
+        end if;
+    end process;
+
+    NEXT_STATE_LOGIC : process(reg_state, reg_channel, reg_data_counter, reg_busy, reg_data_valid, reg_data, reg_cs_n, reg_mosi)
+    begin
+        next_reg_state        <= reg_state;
+        next_reg_channel      <= reg_channel;
+        next_reg_data_counter <= reg_data_counter;
+
+        next_reg_busy       <= reg_busy;
+        next_reg_data_valid <= reg_data_valid;
+        next_reg_data       <= reg_data;
+        next_reg_cs_n       <= reg_cs_n;
+        next_reg_mosi       <= reg_mosi;
 
         case reg_state is
-            when STATE_CS_HIGH =>
+            when STATE_CS_N_HIGH =>
                 if start = '1' then
-                    next_reg_state <= STATE_CS_LOW;
+                    next_reg_channel <= channel;
+                    next_reg_busy    <= '1';
+                    next_reg_cs_n    <= '0';
+
+                    next_reg_state <= STATE_CS_N_LOW;
                 end if;
 
-            when STATE_CS_LOW =>
+            when STATE_CS_N_LOW =>
                 next_reg_state <= STATE_START;
 
             when STATE_START =>
+                -- "first clock received with CS_N low and D_IN high will 
+                -- constiture a start bit".
+                MOSI <= '1';
+                CS_N <= '0';
+                busy <= '1';
+
                 next_reg_state <= STATE_SGL;
 
             when STATE_SGL =>
+                -- SGL / DIFF_N bit (we use SGL)
+                MOSI <= '1';
+                CS_N <= '0';
+                busy <= '1';
+
                 next_reg_state <= STATE_D2;
 
             when STATE_D2 =>
+                -- D2 = don't care
+                CS_N <= '0';
+                busy <= '1';
+
                 next_reg_state <= STATE_D1;
 
             when STATE_D1 =>
+                -- msb of channel
+                MOSI <= reg_channel(1);
+                CS_N <= '0';
+                busy <= '1';
+
                 next_reg_state <= STATE_D0;
 
             when STATE_D0 =>
-                next_reg_state <= STATE_SGL;
+                -- lsb of channel
+                MOSI <= reg_channel(0);
+                CS_N <= '0';
+                busy <= '1';
+
+                next_reg_state <= STATE_SAMPLE;
 
             when STATE_SAMPLE =>
-                next_reg_state <= STATE_SGL;
+                CS_N <= '0';
+                busy <= '1';
+
+                next_reg_state <= STATE_NULL;
 
             when STATE_NULL =>
-                next_reg_state <= STATE_SGL;
+                CS_N                  <= '0';
+                busy                  <= '1';
+                next_reg_data_counter <= to_unsigned(11, next_reg_data_counter'length);
+
+                next_reg_state <= STATE_D_IN;
 
             when STATE_D_IN =>
-                next_reg_state <= STATE_SGL;
+                CS_N                                        <= '0';
+                busy                                        <= '1';
+                next_reg_data_counter                       <= reg_data_counter - 1;
+                next_reg_data(to_integer(reg_data_counter)) <= MISO;
 
+                if reg_data_counter = 0 then
+                    data           <= reg_data;
+                    data_valid     <= '1';
+                    next_reg_state <= STATE_CS_N_HIGH;
+                end if;
         end case;
     end process;
 
--- lab1/hw/hdl/mcp3204/tb/tb_mcp3204_spi.vhd
+++ lab1/hw/hdl/mcp3204/tb/tb_mcp3204_spi.vhd
@@ -12,26 +12,30 @@ architecture rtl of tb_mcp3204_spi is
     signal sim_finished : boolean   := false;
 
     -- mcp3204_spi ------------------------------------------------------------
-    signal busy  : std_logic                     := '0';
-    signal start : std_logic                     := '0';
-    signal done  : std_logic                     := '0';
-    signal data  : std_logic_vector(11 downto 0) := (others => '0');
-    signal CS_N  : std_logic                     := '1';
-    signal MOSI  : std_logic                     := '0';
-    signal MISO  : std_logic                     := '0';
+    signal busy       : std_logic                     := '0';
+    signal start      : std_logic                     := '0';
+    signal channel    : std_logic_vector(1 downto 0)  := (others => '0');
+    signal data_valid : std_logic                     := '0';
+    signal data       : std_logic_vector(11 downto 0) := (others => '0');
+    signal SCLK       : std_logic                     := '0';
+    signal CS_N       : std_logic                     := '1';
+    signal MOSI       : std_logic                     := '0';
+    signal MISO       : std_logic                     := '0';
 
 begin
     duv : entity work.mcp3204_spi
         port map(
-            clk   => clk,
-            reset => reset,
-            busy  => busy,
-            start => start,
-            done  => done,
-            data  => data,
-            CS_N  => CS_N,
-            MOSI  => MOSI,
-            MISO  => MISO
+            clk        => clk,
+            reset      => reset,
+            busy       => busy,
+            start      => start,
+            channel    => channel,
+            data_valid => data_valid,
+            data       => data,
+            SCLK       => SCLK,
+            CS_N       => CS_N,
+            MOSI       => MOSI,
+            MISO       => MISO
         );
 
     clk <= not clk after CLK_PERIOD / 2 when not sim_finished;
@@ -47,10 +51,29 @@ begin
             reset <= '0';
         end procedure async_reset;
 
+        procedure spi_transfer(constant channel_number : natural range 0 to 3) is
+        begin
+            wait until busy = '0';
+
+            wait until falling_edge(clk);
+            start   <= '1';
+            channel <= std_logic_vector(to_unsigned(channel_number, channel'length));
+
+            wait until falling_edge(clk);
+            start   <= '0';
+            channel <= (others => '0');
+
+            wait until rising_edge(data_valid);
+            wait until busy = '0';
+
+        end procedure spi_transfer;
+
     begin
         async_reset;
 
-        sim_finished <= true;
+        spi_transfer(1);
+
+        --        sim_finished <= true;
         wait;
     end process sim;
 end architecture rtl;
