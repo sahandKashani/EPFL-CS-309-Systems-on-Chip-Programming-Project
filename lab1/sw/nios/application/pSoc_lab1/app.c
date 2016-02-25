f8c000332f388088c4fe6d8f0cd7f21bfcf942b3
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2016-02-25 17:18:02 +0100

    [lab1] Dual PWM control for pan-tilt.

--- lab1/sw/nios/application/pSoc_lab1/app.c
+++ lab1/sw/nios/application/pSoc_lab1/app.c
@@ -11,23 +11,35 @@
 /* bottom servo = [10500, 19500] us */
 
 int main(void) {
-    pwm_dev pwm = pwm_inst((void *) PWM_0_BASE);
-    pwm_init(&pwm);
+    pwm_dev pwm0 = pwm_inst((void *) PWM_0_BASE);
+    pwm_dev pwm1 = pwm_inst((void *) PWM_1_BASE);
+    pwm_init(&pwm0);
+    pwm_init(&pwm1);
 
-    uint32_t duty_in_us = 15000;
+    uint32_t duty_in_us0 = 15000;
+    uint32_t duty_in_us1 = 15000;
     uint32_t step = 200;
 
     while (true) {
-        pwm_configure(&pwm, duty_in_us, 25000);
-        pwm_start(&pwm);
+        pwm_configure(&pwm0, duty_in_us0, 25000);
+        pwm_configure(&pwm1, duty_in_us1, 25000);
+        pwm_start(&pwm0);
+        pwm_start(&pwm1);
 
         int c = getc(stdin);
         if (c == 's') {
-            duty_in_us += step;
+            duty_in_us0 += step;
+            printf("duty_in_us0 = %" PRIu32 "\n", duty_in_us0);
         } else if (c == 'a') {
-            duty_in_us -= step;
+            duty_in_us0 -= step;
+            printf("duty_in_us0 = %" PRIu32 "\n", duty_in_us0);
+        } else if (c == 'w') {
+            duty_in_us1 += step;
+            printf("duty_in_us1 = %" PRIu32 "\n", duty_in_us1);
+        } else if (c == 'e') {
+            duty_in_us1 -= step;
+            printf("duty_in_us1 = %" PRIu32 "\n", duty_in_us1);
         }
-        printf("duty_in_us = %" PRIu32 "\n", duty_in_us);
     }
 
     return EXIT_SUCCESS;
