de29bf9fe55d121a3ba0c8a555a1f862395785d2
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-02-28 12:11:18 +0100

    Put PWM register map in separate package with other constants for students to see how to write a clean testbench.
    Fix small bug in testbench where the PWM output can be stuck at '1' when we send a STOP command. We now force the output to '0' at the end of the ongoing period when the PWM is instructed to STOP.
    
    Rename "started" to "run" in pwm.vhd.
    
    Make sure readdata is 0 when reading the write-only control register.

--- TEST_AREA/hw/hdl/pantilt/hdl/pwm.vhd
+++ TEST_AREA/hw/hdl/pantilt/hdl/pwm.vhd
@@ -1,21 +1,13 @@
 -- PWM Memory-Mapped Avalon Slave Interface
 -- Author: Philémon Favrod (philemon.favrod@epfl.ch)
--- Revision: 1
---
--- The register map of the component is shown below:
--- +--------+------------+--------+---------------------------------------------------------------------------+
--- | RegNo  | Name       | Access | Description                                                               |
--- +--------+------------+--------+---------------------------------------------------------------------------+
--- | 0      | PERIOD     | R/W    | Period in clock cycles.                                                   |
--- +--------+------------+--------+---------------------------------------------------------------------------+
--- | 1      | DUTY_CYCLE | R/W    | A value between 0 and PERIOD indicating the duty cycle of the clock.      |
--- +--------+------------+--------+---------------------------------------------------------------------------+
--- | 2      | CTRL       | W      | Writing 0 (resp. 1) to the register stops (resp. starts) the PWM.         |
--- +--------+------------+--------+---------------------------------------------------------------------------+
+-- Modified by: Sahand Kashani-Akhavan (sahand.kashani-akhavan@epfl.ch)
+-- Revision: 2
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
+use work.pwm_constants.all;
 
 entity pwm is
     port(
@@ -38,27 +30,25 @@ entity pwm is
 end pwm;
 
 architecture rtl of pwm is
-    constant REG_PERIOD_OFST     : std_logic_vector(address'range) := std_logic_vector(to_unsigned(0, address'length));
-    constant REG_DUTY_CYCLE_OFST : std_logic_vector(address'range) := std_logic_vector(to_unsigned(1, address'length));
-    constant REG_CTRL_OFST       : std_logic_vector(address'range) := std_logic_vector(to_unsigned(2, address'length));
 
     constant ONE : unsigned(writedata'range) := to_unsigned(1, writedata'length);
 
-    -- Registers
-    -- The versions of the signals prefixed by 'new_' are used to avoid glitches in the PWM.
-    signal period, new_period         : unsigned(31 downto 0);
-    signal duty_cycle, new_duty_cycle : unsigned(31 downto 0);
-    constant DEFAULT_PERIOD           : unsigned(period'range)     := to_unsigned(4, period'length);
-    constant DEFAULT_DUTY_CYCLE       : unsigned(duty_cycle'range) := to_unsigned(2, duty_cycle'length);
+    -- Avalon-MM slave registers
+    -- The versions of the signals prefixed by 'new_' are used to avoid glitches
+    -- in the PWM.
+    signal period, new_period         : unsigned(writedata'range);
+    signal duty_cycle, new_duty_cycle : unsigned(writedata'range);
 
-    -- Internal signals
+    -- Internal registers
     signal counter : unsigned(31 downto 0);
-    signal started : std_logic;
+    signal run     : std_logic;
     signal pwm     : std_logic;
+
 begin
+
     pwm_out <= pwm;
 
-    p_clk_div : process(clk, reset)
+    p_counter : process(clk, reset)
     begin
         if reset = '1' then
             counter <= ONE;
@@ -69,44 +59,46 @@ begin
                 counter <= counter + 1;
             end if;
         end if;
-
-    end process p_clk_div;
+    end process p_counter;
 
     p_pwm : process(clk, reset)
     begin
         if reset = '1' then
             pwm        <= '0';
-            period     <= DEFAULT_PERIOD;
-            duty_cycle <= DEFAULT_DUTY_CYCLE;
+            period     <= to_unsigned(DEFAULT_PERIOD, period'length);
+            duty_cycle <= to_unsigned(DEFAULT_DUTY_CYCLE, duty_cycle'length);
         elsif rising_edge(clk) then
-            if started = '1' then
-                if counter = period then
+            if counter = period then
+                -- Pulse is restarted only if PWM is still running.
+                if run = '1' then
                     pwm        <= '1';
                     period     <= new_period;
                     duty_cycle <= new_duty_cycle;
-                elsif counter >= duty_cycle then
-                    pwm <= '0';
                 end if;
+            elsif counter >= duty_cycle then
+                pwm <= '0';
             end if;
         end if;
-
     end process p_pwm;
 
     p_avalon_write : process(clk, reset)
     begin
         if reset = '1' then
-            new_period     <= DEFAULT_PERIOD;
-            new_duty_cycle <= DEFAULT_DUTY_CYCLE;
-            started        <= '0';
+            new_period     <= to_unsigned(DEFAULT_PERIOD, new_period'length);
+            new_duty_cycle <= to_unsigned(DEFAULT_DUTY_CYCLE, new_duty_cycle'length);
+            run            <= '0';
         elsif rising_edge(clk) then
             if write = '1' then
-                case address is
+                case to_integer(unsigned(address)) is
                     when REG_PERIOD_OFST =>
                         new_period <= unsigned(writedata);
+
                     when REG_DUTY_CYCLE_OFST =>
                         new_duty_cycle <= unsigned(writedata);
+
                     when REG_CTRL_OFST =>
-                        started <= writedata(0);
+                        run <= writedata(0);
+
                     when others => null;
                 end case;
             end if;
@@ -117,12 +109,15 @@ begin
     begin
         if rising_edge(clk) then
             if read = '1' then
-                case address is
+                case to_integer(unsigned(address)) is
                     when REG_PERIOD_OFST =>
                         readdata <= std_logic_vector(period);  -- should technically return new_period
+
                     when REG_DUTY_CYCLE_OFST =>
                         readdata <= std_logic_vector(duty_cycle);  -- should technically return new_duty_cycle
-                    when others => null;
+
+                    when others =>
+                        readdata <= (others => '0');
                 end case;
             end if;
         end if;
---
+++ TEST_AREA/hw/hdl/pantilt/hdl/pwm_constants.vhd
@@ -0,0 +1,61 @@
+-- #############################################################################
+-- pwm_constants.vhd
+-- =================
+-- This package contains constants used in the PWM design files.
+--
+-- Author        : Sahand Kashani-Akhavan [sahand.kashani-akhavan@epfl.ch]
+-- Revision      : 1.0
+-- Last modified : 2017.02.28
+-- #############################################################################
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package pwm_constants is
+    -- Register map
+    -- +--------+------------+--------+------------------------------------------------------------------------------+
+    -- | RegNo  | Name       | Access | Description                                                                  |
+    -- +--------+------------+--------+------------------------------------------------------------------------------+
+    -- | 0      | PERIOD     | R/W    | Period in clock cycles [2 <= period <= (2**32) - 1].                         |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | This value can be read/written while the unit is in the middle of an ongoing |
+    -- |        |            |        | PWM pulse. To allow safe behaviour, one cannot modify the period of an       |
+    -- |        |            |        | ongoing pulse, so we adopt the following semantics for this register:        |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | >> WRITING a value in this register indicates the NEW period to apply to the |
+    -- |        |            |        |    next pulse.                                                               |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | >> READING a value from this register indicates the CURRENT period of the    |
+    -- |        |            |        |    ongoing pulse.                                                            |
+    -- +--------+------------+--------+------------------------------------------------------------------------------+
+    -- | 1      | DUTY_CYCLE | R/W    | Duty cycle of the PWM [1 <= duty cycle <= period]                            |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | This value can be read/written while the unit is in the middle of an ongoing |
+    -- |        |            |        | PWM pulse. To allow safe behaviour, one cannot modify the duty cycle of an   |
+    -- |        |            |        | ongoing pulse, so we adopt the following semantics for this register:        |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | >> WRITING a value in this register indicates the NEW duty cycle to apply to |
+    -- |        |            |        |    the next pulse.                                                           |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | >> READING a value from this register indicates the CURRENT duty cycle of    |
+    -- |        |            |        |    the ongoing pulse.                                                        |
+    -- +--------+------------+--------+------------------------------------------------------------------------------+
+    -- | 2      | CTRL       | WO     | >> Writing 0 to this register stops the PWM once the ongoing pulse has ended.|
+    -- |        |            |        |    Writing 1 to this register starts the PWM.                                |
+    -- |        |            |        |                                                                              |
+    -- |        |            |        | >> Reading this register always returns 0.                                   |
+    -- +--------+------------+--------+------------------------------------------------------------------------------+
+    constant REG_PERIOD_OFST     : natural := 0;
+    constant REG_DUTY_CYCLE_OFST : natural := 1;
+    constant REG_CTRL_OFST       : natural := 2;
+
+    -- Default values of registers after reset (BEFORE writing START to the CTRL
+    -- register with a new configuration)
+    constant DEFAULT_PERIOD     : natural := 4;
+    constant DEFAULT_DUTY_CYCLE : natural := 2;
+end package pwm_constants;
+
+package body pwm_constants is
+
+end package body pwm_constants;
