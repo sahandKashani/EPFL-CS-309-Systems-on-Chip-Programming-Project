33f81e289b755a866e892f52d64054ad9963201c
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-03-14 09:53:57 +0100

    [lab_1_2] PWM fix upper and lower duty cycle values by experimental trials.

--- lab_1_2/lab_1_2_solution/hw/hdl/pantilt/sw/pantilt.h
+++ lab_1_2/lab_1_2_solution/hw/hdl/pantilt/sw/pantilt.h
@@ -13,17 +13,17 @@ typedef struct pantilt_dev {
  *  Public API
  ******************************************************************************/
 
-#define PANTILT_PWM_CLOCK_FREQ_HZ       (50000000) // 50   MHz
+#define PANTILT_PWM_CLOCK_FREQ_HZ       (50000000) // 50.00 MHz
 
-#define PANTILT_PWM_PERIOD_US           (25000)    // 25   ms
-
-/* Horizontal servo */
-#define PANTILT_PWM_V_MIN_DUTY_CYCLE_US (900)      // 0.9  ms
-#define PANTILT_PWM_V_MAX_DUTY_CYCLE_US (2300)     // 2.3  ms
+#define PANTILT_PWM_PERIOD_US           (25000)    // 25.00 ms
 
 /* Vertical servo */
-#define PANTILT_PWM_H_MIN_DUTY_CYCLE_US (1000)     // 1    ms
-#define PANTILT_PWM_H_MAX_DUTY_CYCLE_US (1950)     // 1.95 ms
+#define PANTILT_PWM_V_MIN_DUTY_CYCLE_US (950)      //  0.95 ms
+#define PANTILT_PWM_V_MAX_DUTY_CYCLE_US (2150)     //  2.15 ms
+
+/* Horizontal servo */
+#define PANTILT_PWM_H_MIN_DUTY_CYCLE_US (1000)     //  1.00 ms
+#define PANTILT_PWM_H_MAX_DUTY_CYCLE_US (2000)     //  2.00 ms
 
 pantilt_dev pantilt_inst(void *pwm_v_base, void *pwm_h_base);
 
