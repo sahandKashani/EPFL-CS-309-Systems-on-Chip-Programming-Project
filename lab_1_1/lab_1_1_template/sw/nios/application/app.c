ff8f0cdae77e5cdb2afdc8ebcdee6193997e1c4d
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-03-14 09:53:12 +0100

    [lab_1_1] Add higher-level pantilt.c and pantilt.h files to handle inverted pan tilt rotation (just like joysticks were created around mcp3204).

--- lab_1_1/lab_1_1_template/sw/nios/application/app.c
+++ lab_1_1/lab_1_1_template/sw/nios/application/app.c
@@ -5,69 +5,64 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#include "pwm/pwm.h"
+#include "pantilt/pantilt.h"
 #include "system.h"
 
-#define PWM_CLOCK_FREQ               (50000000)
-#define PWM_PERIOD                   (25000) // 25   ms
-#define PWM_STEP_US                  (10)
+#define SLEEP_DURATION_US (25000) //  25  ms
+#define PANTILT_STEP_US   (25)    //  25  us
 
-// Vertical servo
-#define V_SERVO_MIN_DUTY_CYCLE_US    (900)   // 0.9  ms
-#define V_SERVO_MAX_DUTY_CYCLE_US    (2300)  // 2.3  ms
-
-// Horizontal servo
-#define H_SERVO_MIN_DUTY_CYCLE_US    (1000)  // 1    ms
-#define H_SERVO_MAX_DUTY_CYCLE_US    (1950)  // 1.95 ms
-
-#define SLEEP_DURATION_US            (7500)  // 7.5  ms
+#define PANTILT_PWM_V_CENTER_DUTY_CYCLE_US ((PANTILT_PWM_V_MIN_DUTY_CYCLE_US + PANTILT_PWM_V_MAX_DUTY_CYCLE_US) / 2)
+#define PANTILT_PWM_H_CENTER_DUTY_CYCLE_US ((PANTILT_PWM_H_MIN_DUTY_CYCLE_US + PANTILT_PWM_H_MAX_DUTY_CYCLE_US) / 2)
 
 int main(void) {
-    pwm_dev v_pwm = pwm_inst((void *) PWM_1_BASE);
-    pwm_dev h_pwm = pwm_inst((void *) PWM_0_BASE);
-    pwm_init(&v_pwm);
-    pwm_init(&h_pwm);
+    // Hardware control structures
+    pantilt_dev pantilt = pantilt_inst((void *) PWM_0_BASE, (void *) PWM_1_BASE);
 
-    pwm_configure(&v_pwm, V_SERVO_MIN_DUTY_CYCLE_US, PWM_PERIOD, PWM_CLOCK_FREQ);
-    pwm_configure(&h_pwm, H_SERVO_MIN_DUTY_CYCLE_US, PWM_PERIOD, PWM_CLOCK_FREQ);
-    pwm_start(&v_pwm);
-    pwm_start(&h_pwm);
+    // Initialize hardware
+    pantilt_init(&pantilt);
 
+    // Center servos.
+    pantilt_configure_vertical(&pantilt, PANTILT_PWM_V_MIN_DUTY_CYCLE_US);
+    pantilt_configure_horizontal(&pantilt, PANTILT_PWM_H_MIN_DUTY_CYCLE_US);
+    pantilt_start_vertical(&pantilt);
+    pantilt_start_horizontal(&pantilt);
+
+    // Rotate servos in "square" motion
     while (true) {
         uint32_t v_duty_us = 0;
         uint32_t h_duty_us = 0;
 
-        // top to bottom
-        v_duty_us = V_SERVO_MIN_DUTY_CYCLE_US;
-        do {
-            pwm_configure(&v_pwm, v_duty_us, PWM_PERIOD, PWM_CLOCK_FREQ);
-            v_duty_us += PWM_STEP_US;
-            usleep(SLEEP_DURATION_US);
-        } while (v_duty_us <= V_SERVO_MAX_DUTY_CYCLE_US);
-
-        // right to left
-        h_duty_us = H_SERVO_MIN_DUTY_CYCLE_US;
-        do {
-            pwm_configure(&h_pwm, h_duty_us, PWM_PERIOD, PWM_CLOCK_FREQ);
-            h_duty_us += PWM_STEP_US;
-            usleep(SLEEP_DURATION_US);
-        } while (h_duty_us <= H_SERVO_MAX_DUTY_CYCLE_US);
-
         // bottom to top
-        v_duty_us = V_SERVO_MAX_DUTY_CYCLE_US;
+        v_duty_us = PANTILT_PWM_V_MIN_DUTY_CYCLE_US;
         do {
-            pwm_configure(&v_pwm, v_duty_us, PWM_PERIOD, PWM_CLOCK_FREQ);
-            v_duty_us -= PWM_STEP_US;
+            pantilt_configure_vertical(&pantilt, v_duty_us);
+            v_duty_us += PANTILT_STEP_US;
             usleep(SLEEP_DURATION_US);
-        } while (V_SERVO_MIN_DUTY_CYCLE_US <= v_duty_us);
+        } while (v_duty_us <= PANTILT_PWM_V_MAX_DUTY_CYCLE_US);
 
         // left to right
-        h_duty_us = H_SERVO_MAX_DUTY_CYCLE_US;
+        h_duty_us = PANTILT_PWM_H_MIN_DUTY_CYCLE_US;
         do {
-            pwm_configure(&h_pwm, h_duty_us, PWM_PERIOD, PWM_CLOCK_FREQ);
-            h_duty_us -= PWM_STEP_US;
+            pantilt_configure_horizontal(&pantilt, h_duty_us);
+            h_duty_us += PANTILT_STEP_US;
             usleep(SLEEP_DURATION_US);
-        } while (H_SERVO_MIN_DUTY_CYCLE_US <= h_duty_us);
+        } while (h_duty_us <= PANTILT_PWM_H_MAX_DUTY_CYCLE_US);
+
+        // top to bottom
+        v_duty_us = PANTILT_PWM_V_MAX_DUTY_CYCLE_US;
+        do {
+            pantilt_configure_vertical(&pantilt, v_duty_us);
+            v_duty_us -= PANTILT_STEP_US;
+            usleep(SLEEP_DURATION_US);
+        } while (PANTILT_PWM_V_MIN_DUTY_CYCLE_US <= v_duty_us);
+
+        // left to right
+        h_duty_us = PANTILT_PWM_H_MAX_DUTY_CYCLE_US;
+        do {
+            pantilt_configure_horizontal(&pantilt, h_duty_us);
+            h_duty_us -= PANTILT_STEP_US;
+            usleep(SLEEP_DURATION_US);
+        } while (PANTILT_PWM_H_MIN_DUTY_CYCLE_US <= h_duty_us);
     }
 
     return EXIT_SUCCESS;
