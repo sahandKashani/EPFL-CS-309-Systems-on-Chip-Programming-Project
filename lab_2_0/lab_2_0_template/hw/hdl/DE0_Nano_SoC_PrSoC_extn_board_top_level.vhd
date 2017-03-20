189a3c86b09a4f8798efb3657b57fb6ce11a7842
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-03-20 09:43:37 +0100

    [lab_2_0] Template (copied from solution for now, must trim down).

---
+++ lab_2_0/lab_2_0_template/hw/hdl/DE0_Nano_SoC_PrSoC_extn_board_top_level.vhd
@@ -0,0 +1,203 @@
+-- #############################################################################
+-- DE0_Nano_SoC_PrSoC_extn_board_top_level.vhd
+--
+-- BOARD         : PrSoC extension board for DE0-Nano-SoC
+-- Author        : Florian Depraz based on Sahand Kashani-Akhavan work
+-- Revision      : 1.1
+-- Creation date : 06/02/2016
+--
+-- Syntax Rule : GROUP_NAME_N[bit]
+--
+-- GROUP : specify a particular interface (ex: SDR_)
+-- NAME  : signal name (ex: CONFIG, D, ...)
+-- bit   : signal index
+-- _N    : to specify an active-low signal
+-- #############################################################################
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DE0_Nano_SoC_PrSoC_extn_board_top_level is
+    port(
+        -------------------------------
+        -- Comment ALL unused ports. --
+        -------------------------------
+
+        -- CLOCK
+        FPGA_CLK1_50 : in std_logic;
+     -- FPGA_CLK2_50 : in std_logic;
+     -- FPGA_CLK3_50 : in std_logic;
+
+        -- KEY on DE0 Nano SoC
+        KEY_N : in std_logic_vector(1 downto 0);
+
+        -- LEDs on DE0 Nano SoC
+     -- LED : out std_logic_vector(7 downto 0);
+
+        -- SWITCHES on DE0 Nano SoC
+     -- SW : in std_logic_vector(3 downto 0);
+
+        -- Servomotors pwm
+        SERVO_0 : out std_logic;
+        SERVO_1 : out std_logic;
+
+        -- ADC Joysticks
+        J0_SPI_CS_n : out std_logic;
+        J0_SPI_MOSI : out std_logic;
+        J0_SPI_MISO : in  std_logic;
+        J0_SPI_CLK  : out std_logic;
+
+        -- Lepton
+        CAM_TH_SPI_CS_N : out std_logic;
+        CAM_TH_MISO     : in  std_logic;
+        CAM_TH_MOSI     : out std_logic;
+        CAM_TH_CLK      : out std_logic
+
+        -- PCA9637
+     -- PIO_SCL   : inout std_logic;
+     -- PIO_SDA   : inout std_logic;
+     -- PIO_INT_N : in    std_logic;
+     -- RESET_N   : out   std_logic;
+
+        -- OV7670
+     -- CAM_D       : in  std_logic_vector(9 downto 0);
+     -- CAM_PIX_CLK : in  std_logic;
+     -- CAM_LV      : in  std_logic;
+     -- CAM_FV      : in  std_logic;
+     -- CAM_SYS_CLK : out std_logic;
+
+        -- VGA and LCD shared signals
+     -- VIDEO_CLK   : out std_logic;
+     -- VIDEO_VSYNC : out std_logic;
+     -- VIDEO_HSYNC : out std_logic;
+     -- VIDEO_B     : out std_logic_vector(7 downto 0);
+     -- VIDEO_G     : out std_logic_vector(7 downto 0);
+     -- VIDEO_R     : out std_logic_vector(7 downto 0);
+
+        -- LCD Specific signals
+     -- LCD_DE         : out   std_logic;
+     -- LCD_PIN_DAV_N  :       ? ?? std_logic;
+     -- LCD_DISPLAY_EN : out   std_logic;
+     -- SPI_MISO       : in    std_logic;
+     -- SPI_ENA_N      : out   std_logic;
+     -- SPI_CLK        : out   std_logic;
+     -- SPI_MOSI       : out   std_logic;
+     -- SPI_DAT        : inout std_logic;
+
+        -- I2C TOUCH SCREEN
+     -- TS_SCL : inout std_logic;
+     -- TS_SDA : inout std_logic;
+
+        -- BLUETOOTH (BLE)
+     -- BLT_TXD : in  std_logic;
+     -- BLT_RXD : out std_logic;
+
+        -- I2C For VGA, PAL and OV7670 cameras
+     -- CAM_PAL_VGA_SDA : inout std_logic;
+     -- CAM_PAL_VGA_SCL : inout std_logic;
+
+        -- ONE WIRE
+     -- BOARD_ID : inout std_logic;
+
+        -- PAL Camera
+     -- PAL_VD_VD   : in  std_logic_vector(7 downto 0);
+     -- PAL_VD_VSO  : in  std_logic;
+     -- PAL_VD_HSO  : in  std_logic;
+     -- PAL_VD_CLKO : in  std_logic;
+     -- PAL_PWDN    : out std_logic;
+
+        -- WIFI
+     -- FROM_ESP_TXD : in  std_logic;
+     -- TO_ESP_RXD   : out std_logic;
+
+        -- LED RGB
+     -- LED_BGR : out std_logic;
+
+        -- HPS
+     -- HPS_CONV_USB_N   : inout std_logic;
+     -- HPS_DDR3_ADDR    : out   std_logic_vector(14 downto 0);
+     -- HPS_DDR3_BA      : out   std_logic_vector(2 downto 0);
+     -- HPS_DDR3_CAS_N   : out   std_logic;
+     -- HPS_DDR3_CK_N    : out   std_logic;
+     -- HPS_DDR3_CK_P    : out   std_logic;
+     -- HPS_DDR3_CKE     : out   std_logic;
+     -- HPS_DDR3_CS_N    : out   std_logic;
+     -- HPS_DDR3_DM      : out   std_logic_vector(3 downto 0);
+     -- HPS_DDR3_DQ      : inout std_logic_vector(31 downto 0);
+     -- HPS_DDR3_DQS_N   : inout std_logic_vector(3 downto 0);
+     -- HPS_DDR3_DQS_P   : inout std_logic_vector(3 downto 0);
+     -- HPS_DDR3_ODT     : out   std_logic;
+     -- HPS_DDR3_RAS_N   : out   std_logic;
+     -- HPS_DDR3_RESET_N : out   std_logic;
+     -- HPS_DDR3_RZQ     : in    std_logic;
+     -- HPS_DDR3_WE_N    : out   std_logic;
+     -- HPS_ENET_GTX_CLK : out   std_logic;
+     -- HPS_ENET_INT_N   : inout std_logic;
+     -- HPS_ENET_MDC     : out   std_logic;
+     -- HPS_ENET_MDIO    : inout std_logic;
+     -- HPS_ENET_RX_CLK  : in    std_logic;
+     -- HPS_ENET_RX_DATA : in    std_logic_vector(3 downto 0);
+     -- HPS_ENET_RX_DV   : in    std_logic;
+     -- HPS_ENET_TX_DATA : out   std_logic_vector(3 downto 0);
+     -- HPS_ENET_TX_EN   : out   std_logic;
+     -- HPS_GSENSOR_INT  : inout std_logic;
+     -- HPS_I2C0_SCLK    : inout std_logic;
+     -- HPS_I2C0_SDAT    : inout std_logic;
+     -- HPS_I2C1_SCLK    : inout std_logic;
+     -- HPS_I2C1_SDAT    : inout std_logic;
+     -- HPS_KEY_N        : inout std_logic;
+     -- HPS_LED          : inout std_logic;
+     -- HPS_LTC_GPIO     : inout std_logic;
+     -- HPS_SD_CLK       : out   std_logic;
+     -- HPS_SD_CMD       : inout std_logic;
+     -- HPS_SD_DATA      : inout std_logic_vector(3 downto 0);
+     -- HPS_SPIM_CLK     : out   std_logic;
+     -- HPS_SPIM_MISO    : in    std_logic;
+     -- HPS_SPIM_MOSI    : out   std_logic;
+     -- HPS_SPIM_SS      : inout std_logic;
+     -- HPS_UART_RX      : in    std_logic;
+     -- HPS_UART_TX      : out   std_logic;
+     -- HPS_USB_CLKOUT   : in    std_logic;
+     -- HPS_USB_DATA     : inout std_logic_vector(7 downto 0);
+     -- HPS_USB_DIR      : in    std_logic;
+     -- HPS_USB_NXT      : in    std_logic;
+     -- HPS_USB_STP      : out   std_logic
+    );
+end entity DE0_Nano_SoC_PrSoC_extn_board_top_level;
+
+architecture rtl of DE0_Nano_SoC_PrSoC_extn_board_top_level is
+    component soc_system is
+        port (
+            clk_clk                    : in  std_logic := 'X';
+            reset_reset_n              : in  std_logic := 'X';
+            pwm_0_conduit_end_pwm      : out std_logic;
+            pwm_1_conduit_end_pwm      : out std_logic;
+            mcp3204_0_conduit_end_cs_n : out std_logic;
+            mcp3204_0_conduit_end_mosi : out std_logic;
+            mcp3204_0_conduit_end_miso : in  std_logic := 'X';
+            mcp3204_0_conduit_end_sclk : out std_logic;
+            lepton_0_spi_cs_n          : out std_logic;
+            lepton_0_spi_mosi          : out std_logic;
+            lepton_0_spi_miso          : in  std_logic := 'X';
+            lepton_0_spi_sclk          : out std_logic
+        );
+    end component soc_system;
+
+begin
+    soc_system_inst : component soc_system
+    port map (
+        clk_clk                    => FPGA_CLK1_50,
+        reset_reset_n              => KEY_N(0),
+        pwm_0_conduit_end_pwm      => SERVO_0,
+        pwm_1_conduit_end_pwm      => SERVO_1,
+        mcp3204_0_conduit_end_cs_n => J0_SPI_CS_n,
+        mcp3204_0_conduit_end_mosi => J0_SPI_MOSI,
+        mcp3204_0_conduit_end_miso => J0_SPI_MISO,
+        mcp3204_0_conduit_end_sclk => J0_SPI_CLK,
+        lepton_0_spi_cs_n          => CAM_TH_SPI_CS_N,
+        lepton_0_spi_mosi          => CAM_TH_MOSI,
+        lepton_0_spi_miso          => CAM_TH_MISO,
+        lepton_0_spi_sclk          => CAM_TH_CLK
+    );
+
+end;
---
+++ lab_2_0/lab_2_0_template/hw/hdl/lepton/hdl/lepton.vhd
@@ -0,0 +1,287 @@
+-- Lepton Avalon Memory-Mapped Slave Interface
+-- Author: Philémon Favrod (philemon.favrod@epfl.ch)
+-- Modified by: Sahand Kashani-Akhavan (sahand.kashani-akhavan@epfl.ch)
+-- Revision: 2
+
+-- Register map
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- | RegNo         | Name            | Access | Description                                       |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             0 | COMMAND         | WO     | Command                                           |
+-- |               |                 |        | - Writing 1 starts capturing a frame.             |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             1 | STATUS          | RO     | Status                                            |
+-- |               |                 |        | - Bit 0: 0 --> no capture in progress.            |
+-- |               |                 |        |          1 --> capture in progress.               |
+-- |               |                 |        | - Bit 1: 0 --> previous capture successful.       |
+-- |               |                 |        |          1 --> error during previous capture.     |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             2 | MIN             | RO     | Minimum pixel value in frame.                     |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             3 | MAX             | RO     | Maximum pixel value in frame.                     |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             4 | SUM_LSB         | RO     | Sum of all pixels in frame (low 16 bits).         |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             5 | SUM_MSB         | RO     | Sum of all pixels in frame (high 16 bits).        |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             6 | ROW_IDX         | RO     | Current line being captured (1 <= ROW_IDX <= 60). |
+-- |               |                 |        | Available for debugging purposes.                 |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |             7 | RESERVED        | -      | Reserved                                          |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |     8 -  4807 | RAW BUFFER      | RO     | View into RAW pixel buffer.                       |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |  4808 -  8191 | RESERVED        | -      | Reserved                                          |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- |  8192 - 12991 | ADJUSTED BUFFER | RO     | View into adjusted (scaled) pixel buffer.         |
+-- |               |                 |        | Values are scaled between MIN and MAX.            |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+-- | 12991 - 16383 | RESERVED        | -      | Reserved                                          |
+-- +---------------+-----------------+--------+---------------------------------------------------+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity lepton is
+    port(
+        clk       : in  std_logic;
+        reset     : in  std_logic;
+        address   : in  std_logic_vector(13 downto 0);
+        readdata  : out std_logic_vector(15 downto 0);
+        writedata : in  std_logic_vector(15 downto 0);
+        read      : in  std_logic;
+        write     : in  std_logic;
+
+        SCLK : out std_logic;
+        CSn  : out std_logic;
+        MOSI : out std_logic;
+        MISO : in  std_logic
+    );
+
+end lepton;
+
+architecture rtl of lepton is
+    signal spi_cs_n             : std_logic;
+    signal spi_mosi_data        : std_logic_vector(7 downto 0);
+    signal spi_mosi_valid       : std_logic;
+    signal spi_mosi_ready       : std_logic;
+    signal spi_miso_data        : std_logic_vector(7 downto 0);
+    signal spi_miso_valid       : std_logic;
+    signal lepton_manager_start : std_logic;
+    signal lepton_manager_error : std_logic;
+    signal byte_data            : std_logic_vector(7 downto 0);
+    signal byte_valid           : std_logic;
+    signal byte_sof             : std_logic;
+    signal byte_eof             : std_logic;
+    signal pix_data             : std_logic_vector(13 downto 0);
+    signal pix_valid            : std_logic;
+    signal pix_sof              : std_logic;
+    signal pix_eof              : std_logic;
+    signal stat_min             : std_logic_vector(13 downto 0);
+    signal stat_max             : std_logic_vector(13 downto 0);
+    signal stat_sum             : std_logic_vector(26 downto 0);
+    signal stat_valid           : std_logic;
+    signal ram_data             : std_logic_vector(15 downto 0);
+    signal ram_wren             : std_logic;
+    signal ram_wraddress        : std_logic_vector(12 downto 0);
+    signal ram_rdaddress        : std_logic_vector(12 downto 0);
+    signal ram_q                : std_logic_vector(15 downto 0);
+    signal row_idx              : std_logic_vector(5 downto 0);
+    signal raw_pixel            : std_logic_vector(13 downto 0);
+    signal raw_max              : std_logic_vector(13 downto 0);
+    signal raw_min              : std_logic_vector(13 downto 0);
+    signal raw_sum              : std_logic_vector(26 downto 0);
+    signal adjusted_pixel       : std_logic_vector(13 downto 0);
+
+    constant COMMAND_REG_OFFSET         : std_logic_vector(address'range) := "00000000000000";
+    constant STATUS_REG_OFFSET          : std_logic_vector(address'range) := "00000000000001";
+    constant MIN_REG_OFFSET             : std_logic_vector(address'range) := "00000000000010";
+    constant MAX_REG_OFFSET             : std_logic_vector(address'range) := "00000000000011";
+    constant SUM_LSB_REG_OFFSET         : std_logic_vector(address'range) := "00000000000100";
+    constant SUM_MSB_REG_OFFSET         : std_logic_vector(address'range) := "00000000000101";
+    constant ROW_IDX_REG_OFFSET         : std_logic_vector(address'range) := "00000000000110";
+    constant BUFFER_REG_OFFSET          : unsigned(address'range)         := "00000000001000";
+    constant ADJUSTED_BUFFER_REG_OFFSET : unsigned(address'range)         := "10000000000000";
+
+    constant IMAGE_SIZE       : integer                 := 80 * 60;
+    constant BUFFER_REG_LIMIT : unsigned(address'range) := unsigned(BUFFER_REG_OFFSET) + IMAGE_SIZE;
+
+    constant ADJUSTED_BUFFER_LIMIT : unsigned(address'range) := unsigned(ADJUSTED_BUFFER_REG_OFFSET) + IMAGE_SIZE;
+
+    signal max_reg   : std_logic_vector(stat_max'range);
+    signal min_reg   : std_logic_vector(stat_min'range);
+    signal sum_reg   : std_logic_vector(stat_sum'range);
+    signal error_reg : std_logic;
+
+begin
+    spi_controller0 : entity work.avalon_st_spi_master
+    port map(
+        clk             => clk,
+        reset           => reset,
+        spi_cs_n        => spi_cs_n,
+        mosi_sink_data  => spi_mosi_data,
+        mosi_sink_valid => spi_mosi_valid,
+        mosi_sink_ready => spi_mosi_ready,
+        miso_src_data   => spi_miso_data,
+        miso_src_valid  => spi_miso_valid,
+        SCLK            => SCLK,
+        MISO            => MISO,
+        MOSI            => MOSI,
+        CS_n            => CSn
+    );
+
+    lepton_manager0 : entity work.lepton_manager
+    port map(
+        clk                 => clk,
+        reset               => reset,
+        spi_miso_sink_data  => spi_miso_data,
+        spi_miso_sink_valid => spi_miso_valid,
+        spi_mosi_src_data   => spi_mosi_data,
+        spi_mosi_src_valid  => spi_mosi_valid,
+        spi_mosi_src_ready  => spi_mosi_ready,
+        lepton_out_data     => byte_data,
+        lepton_out_valid    => byte_valid,
+        lepton_out_sof      => byte_sof,
+        lepton_out_eof      => byte_eof,
+        row_idx             => row_idx,
+        error               => lepton_manager_error,
+        start               => lepton_manager_start,
+        spi_cs_n            => spi_cs_n
+    );
+
+    byte2pix0 : entity work.byte2pix
+    port map(
+        clk        => clk,
+        reset      => reset,
+        byte_data  => byte_data,
+        byte_valid => byte_valid,
+        byte_sof   => byte_sof,
+        byte_eof   => byte_eof,
+        pix_data   => pix_data,
+        pix_valid  => pix_valid,
+        pix_sof    => pix_sof,
+        pix_eof    => pix_eof
+    );
+
+    lepton_stats0 : entity work.lepton_stats
+    port map(
+        reset      => reset,
+        clk        => clk,
+        pix_data   => pix_data,
+        pix_valid  => pix_valid,
+        pix_sof    => pix_sof,
+        pix_eof    => pix_eof,
+        stat_min   => stat_min,
+        stat_max   => stat_max,
+        stat_sum   => stat_sum,
+        stat_valid => stat_valid
+    );
+
+    ram_writer0 : entity work.ram_writer
+    port map(
+        clk           => clk,
+        reset         => reset,
+        pix_data      => pix_data,
+        pix_valid     => pix_valid,
+        pix_sof       => pix_sof,
+        pix_eof       => pix_eof,
+        ram_data      => ram_data,
+        ram_wren      => ram_wren,
+        ram_wraddress => ram_wraddress
+    );
+
+    dual_ported_ram0 : entity work.dual_ported_ram
+    port map(
+        clock     => clk,
+        data      => ram_data,
+        rdaddress => ram_rdaddress,
+        wraddress => ram_wraddress,
+        wren      => ram_wren,
+        q         => ram_q
+    );
+
+    level_adjuster0 : entity work.level_adjuster
+    port map(
+        clk            => clk,
+        raw_pixel      => ram_q(13 downto 0),
+        raw_max        => max_reg,
+        raw_min        => min_reg,
+        raw_sum        => sum_reg,
+        adjusted_pixel => adjusted_pixel
+    );
+
+    p_lepton_start : process(clk, reset)
+    begin
+        if reset = '1' then
+            lepton_manager_start <= '0';
+            error_reg            <= '0';
+        elsif rising_edge(clk) then
+            if write = '1' and address = COMMAND_REG_OFFSET then
+                lepton_manager_start <= writedata(0);
+                error_reg            <= '0';
+            elsif pix_eof = '1' then
+                lepton_manager_start <= '0';
+            elsif lepton_manager_error = '1' then
+                error_reg <= '1';
+            end if;
+        end if;
+    end process p_lepton_start;
+
+    p_stat_reg : process(clk, reset)
+    begin
+        if reset = '1' then
+            min_reg <= (others => '0');
+            max_reg <= (others => '0');
+            sum_reg <= (others => '0');
+        elsif rising_edge(clk) then
+            if stat_valid = '1' then
+                min_reg <= stat_min;
+                max_reg <= stat_max;
+                sum_reg <= stat_sum;
+            end if;
+        end if;
+    end process p_stat_reg;
+
+    p_read : process(clk, reset)
+    begin
+        if reset = '1' then
+            readdata      <= (others => '0');
+            ram_rdaddress <= (others => '0');
+        elsif rising_edge(clk) then
+            readdata <= (others => '0');
+            if read = '1' then
+                case address is
+                    when STATUS_REG_OFFSET =>
+                        readdata(1) <= error_reg;
+                        readdata(0) <= lepton_manager_start;
+
+                    when MIN_REG_OFFSET =>
+                        readdata <= "00" & min_reg;
+
+                    when MAX_REG_OFFSET =>
+                        readdata <= "00" & max_reg;
+
+                    when SUM_MSB_REG_OFFSET =>
+                        readdata <= "00000" & sum_reg(26 downto 16);
+
+                    when SUM_LSB_REG_OFFSET =>
+                        readdata <= sum_reg(15 downto 0);
+
+                    when ROW_IDX_REG_OFFSET =>
+                        readdata(5 downto 0) <= row_idx;
+
+                    when others =>
+                        if unsigned(address) >= BUFFER_REG_OFFSET and unsigned(address) < BUFFER_REG_LIMIT then
+                            ram_rdaddress <= std_logic_vector(resize(unsigned(address) - BUFFER_REG_OFFSET, ram_rdaddress'length));
+                            readdata      <= ram_q;
+                        elsif unsigned(address) >= ADJUSTED_BUFFER_REG_OFFSET and unsigned(address) < ADJUSTED_BUFFER_LIMIT then
+                            ram_rdaddress <= std_logic_vector(resize(unsigned(address) - ADJUSTED_BUFFER_REG_OFFSET, ram_rdaddress'length));
+                            readdata      <= "00" & adjusted_pixel;
+                        end if;
+                end case;
+            end if;
+        end if;
+    end process p_read;
+
+end rtl;
