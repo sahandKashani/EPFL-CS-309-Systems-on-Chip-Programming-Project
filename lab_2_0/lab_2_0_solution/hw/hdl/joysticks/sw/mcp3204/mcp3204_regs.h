d3094241a8e6617849c56d01f1a1fd2fe9bba86d
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-05-15 08:29:01 +0200

    Remove hdl/.../sw/ directories from all lab solutions. These files are already available in the sw/ folder

--- lab_2_0/lab_2_0_solution/hw/hdl/joysticks/sw/mcp3204/mcp3204_regs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MCP3204_REGS_H__
-#define __MCP3204_REGS_H__
-
-#define MCP3204_CHANNEL_0_OFST (0 * 4) /* RO */
-#define MCP3204_CHANNEL_1_OFST (1 * 4) /* RO */
-#define MCP3204_CHANNEL_2_OFST (2 * 4) /* RO */
-#define MCP3204_CHANNEL_3_OFST (3 * 4) /* RO */
-
-#endif /* __MCP3204_REGS_H__ */
