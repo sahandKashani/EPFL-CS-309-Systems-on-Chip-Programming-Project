562c3a7eee9d78ba2e7843a3dbc5b9e7e7651101
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-05-15 12:23:51 +0200

    [lab_4_0] Update config_system.sh to supply a static IP directly to the board.

--- lab_4_0/lab_4_0_solution/sw/hps/linux/rootfs/config_system.sh
+++ lab_4_0/lab_4_0_solution/sw/hps/linux/rootfs/config_system.sh
@@ -26,7 +26,10 @@ iface lo inet loopback
 
 # The primary network interface
 auto eth0
-iface eth0 inet dhcp
+iface eth0 inet static
+address 10.42.0.2
+netmask 255.255.255.0
+gateway 10.42.0.1
 EOF
 
 # DNS configuration for name resolution. We use google's public DNS server here.
